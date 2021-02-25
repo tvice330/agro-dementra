a92e2db92bc9d08ffafeebb4e44f640629c837e3
tvice330 <tvice330@gmail.com>
2021-02-25 18:17:39 +0200

    Task 7 and 9

--- database/seeds/ActionSeeder.php
+++ database/seeds/ActionSeeder.php
@@ -11,22 +11,27 @@ class ActionSeeder extends Seeder
         $action = Action::create([
             'title' => 'АКЦИЯ! На каждый гектар посева подарок:'
         ]);
+
         ActionValue::create([
             'action_id' => $action->id,
             'name' => '50 кг. органического удобрения (граннулированный куриный помет);'
         ]);
+
         ActionValue::create([
             'action_id' => $action->id,
             'name' => 'ИЛИ Стаффер бор Блек 1 л.;'
         ]);
+
         ActionValue::create([
             'action_id' => $action->id,
             'name' => 'ИЛИ Стаффер эликсир;'
         ]);
+
         ActionValue::create([
             'action_id' => $action->id,
             'name' => 'ИЛИ Стаффер стандарт;'
         ]);
+
         ActionValue::create([
             'action_id' => $action->id,
             'name' => 'ИЛИ отсрочка платежа 30/70%;'
