f1ea08db3c5c4d4c296214def1c28cb549870a24
tvice330 <tvice330@gmail.com>
2021-03-10 23:05:29 +0200

    task_16

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::apiResource('menus', 'Admin\Menu\MenuController');
-Route::apiResource('settings', 'Admin\Setting\SettingController');
-Route::apiResource('advantages', 'Admin\Advantage\AdvantageController');
-Route::apiResource('categories', 'Admin\Category\CategoryController');
-Route::apiResource('partnerships', 'Admin\Partnership\PartnershipController');
-Route::apiResource('user_calls', 'Admin\UserCall\UserCallController');
-Route::apiResource('actions', 'Admin\Action\ActionController');
-Route::apiResource('action_values', 'Admin\ActionValues\ActionValuesController');
-Route::apiResource('company_descriptions', 'Admin\CompanyDescription\CompanyDescriptionController');
+Route::namespace('Admin')->prefix('/Admin')->group(function () {
+    Route::apiResource('menus', 'Admin\Menu\MenuController');
+    Route::apiResource('settings', 'Admin\Setting\SettingController');
+    Route::apiResource('advantages', 'Admin\Advantage\AdvantageController');
+    Route::apiResource('categories', 'Admin\Category\CategoryController');
+    Route::apiResource('partnerships', 'Admin\Partnership\PartnershipController');
+    Route::apiResource('user_calls', 'Admin\UserCall\UserCallController');
+    Route::apiResource('actions', 'Admin\Action\ActionController');
+    Route::apiResource('action_values', 'Admin\ActionValues\ActionValuesController');
+    Route::apiResource('company_descriptions', 'Admin\CompanyDescription\CompanyDescriptionController');
+});
